701b901a868841382eac91f6f0be86444a8b90b6
Emily Zheng <mzheng9@u.rochester.edu>
2020-12-06 23:51:19 -0500

    Main Page is displaying products; moved 'instance' into 'static'; need to fix alignment

--- 210project/project/__init__.py
+++ 210project/project/__init__.py
@@ -2,24 +2,28 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager 
+from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
 
+
 def create_app():
     app = Flask(__name__)
+    app.run(debug=True, use_reloader=False)
     bootstrap = Bootstrap(app)
 
     app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
 
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.init_app(app)
 
     # with app.app_context():
     #     init_db()
-        
+
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
--- 210project/project/main.py
+++ 210project/project/main.py
@@ -6,18 +6,21 @@ from flask_login import login_required, current_user
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired
 from wtforms import StringField, SubmitField, PasswordField, BooleanField, FileField
-from wtforms.validators import DataRequired, EqualTo, Email, Length 
+from wtforms.validators import DataRequired, EqualTo, Email, Length
 from . import db as db
 
 
 from project import auth, models
 
 main = Blueprint('main', __name__)
+# UPLOAD_FOLDER = os.path.join(current_app.root_path, 'instance', 'photos')
 
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    from .models import Product
+    return render_template('index.html', products=Product.query.all())
+
 
 @main.route('/profile', methods=['GET', 'POST'])
 @login_required
@@ -29,33 +32,32 @@ def profile():
 
         # print("PATH = ", os.path.join(current_app.root_path)
         # download the photo
-        photo.save(os.path.join(current_app.root_path, 'instance', 'photos', filename))
+        photo.save(os.path.join('static',
+                                'instance', 'photos', filename))
 
         title = product_form.title.data
         description = product_form.description.data
         price = product_form.price.data
         price = int(price)
         photo_name = filename
-        
+
         from .models import User
         author_id = User.query.filter_by(name=current_user.name).first().id
 
         # print("price =", price, "type = ", type(price))
 
         # print("got them")
-        
-        product = models.Product(title=title, description=description, \
-        price=price, photo_name=photo_name, author_id=author_id)
+
+        product = models.Product(title=title, description=description,
+                                 price=price, photo_name=photo_name, author_id=author_id)
         # print("product created", title, description, price, photo_path)
         # print("product = ", product, product.title, product.description, product.price, product.photo_path)
-        
+
         from .models import Product
         db.create_all()
         db.session.add(product)
         db.session.commit()
-        
-        return redirect(url_for('main.index'))
-        
-    return render_template('profile.html', product_form=product_form, name=current_user.name)
 
-   
+        return redirect(url_for('main.index'))
+
+    return render_template('profile.html', product_form=product_form, name=current_user.name)
